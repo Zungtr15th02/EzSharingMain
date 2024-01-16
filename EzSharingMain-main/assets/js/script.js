575a9030b1e212219519402ba12bf6f8523d7371
Zungtr15th02 <135204991+Zungtr15th02@users.noreply.github.com>
2024-01-16 17:33:32 +0700

    Add files via upload

---
+++ EzSharingMain-main/assets/js/script.js
@@ -0,0 +1,69 @@
+'use strict';
+
+/**
+ * add event on element
+ */
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+}
+
+
+
+/** 
+ * toggle navbar
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  navToggler.classList.toggle("active");
+}
+
+addEventOnElem(navToggler, "click", toggleNavbar);
+
+const closeNavbar = function () {
+  navbar.classList.remove("active");
+  navToggler.classList.remove("active");
+}
+
+addEventOnElem(navbarLinks, "click", closeNavbar);
+
+
+
+/**
+ * header active
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
+
+// faq
+function toggleAnswer(index) {
+  const faqQuestion = document.querySelectorAll('.faq-question')[index - 1];
+  const faqAnswer = document.querySelectorAll('.faq-answer')[index - 1];
+
+  faqQuestion.classList.toggle('active');
+  faqAnswer.style.maxHeight = faqQuestion.classList.contains('active') ? faqAnswer.scrollHeight + 'px' : 0;
+}
+// end faq
+
